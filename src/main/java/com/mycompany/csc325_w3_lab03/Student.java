3f04d0ae505d8615a13ce3dd3e7f0040313c7f25
Christopher Canenguez <canenguez.chris@gmail.com>
2022-09-14 10:03:08 -0400

    Added setter and getter methods to the Student Class.

--- src/main/java/com/mycompany/csc325_w3_lab03/Student.java
+++ src/main/java/com/mycompany/csc325_w3_lab03/Student.java
@@ -13,6 +13,30 @@ public class Student
     String firstName;
     String lastName;
     double gpa;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
     
     
     
