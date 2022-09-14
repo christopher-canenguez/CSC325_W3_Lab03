cf3ecd0959a135188781f335c37a3a5de121fd94
Christopher Canenguez <canenguez.chris@gmail.com>
2022-09-14 11:14:24 -0400

    Created the Freshman class that will inherit from Student class.

---
+++ src/main/java/com/mycompany/csc325_w3_lab03/Freshman.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.csc325_w3_lab03;
+
+/**
+ *
+ * @author chriscanenguez
+ */
+public class Freshman {
+    
+}
